d38e81e2ba9517707286b6a73a62461b0453a650
Suraj Yadav <ysuraj877@gmail.com>
2023-10-16 12:42:35 +0000

    Add node adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
+import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: 'hybrid',
+  adapter: nodejs({
+    mode: 'middleware'
+  }),
 });
