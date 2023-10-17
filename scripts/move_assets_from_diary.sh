49df2daeb94b1c572108f7bb11d72c169367705f
Suraj Yadav <ysuraj877@gmail.com>
2023-10-17 11:14:12 +0530

    Add script & pre hooks for diary assets

---
+++ scripts/move_assets_from_diary.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Define the source and destination directories
+src_dir="./src/pages/diary/"
+dest_dir="./public/"
+
+# Use find to copy all files except index.md
+find $src_dir -type f ! -name 'index.md' -exec cp {} $dest_dir \;
+
+# Then use find to delete the copied files from the source directory
+find $src_dir -type f ! -name 'index.md' -exec rm {} \;
