fa6bf94861b5430ec3d3c3f6d12c78995cb0175a
Suraj Yadav <ysuraj877@gmail.com>
2023-10-17 11:56:31 +0530

    Ignore gitfiles while migrating assets

--- scripts/move_assets_from_diary.sh
+++ scripts/move_assets_from_diary.sh
@@ -3,8 +3,11 @@
 src_dir="./src/submodules/diary/"
 dest_dir="./public/"
 
+# make public dir
+make -p ./public
+
 # Use find to copy all files except index.md
-find $src_dir -type f ! -name 'index.md' -exec cp {} $dest_dir \;
+find $src_dir -type f ! -name 'index.md' ! -name .git ! -name .gitignore -exec cp {} $dest_dir \;
 
 # Then use find to delete the copied files from the source directory
-find $src_dir -type f ! -name 'index.md' -exec rm {} \;
+find $src_dir -type f ! -name 'index.md' ! -name .git ! -name .gitignore -exec rm {} \;
