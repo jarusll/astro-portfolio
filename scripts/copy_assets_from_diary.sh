cc435651aea339fa0cb44bc21e8150565ae30d90
Suraj Yadav <ysuraj877@gmail.com>
2023-10-17 14:42:29 +0000

    Only copy assets from diary

--- scripts/copy_assets_from_diary.sh
+++ scripts/copy_assets_from_diary.sh
@@ -8,6 +8,3 @@ make -p ./public
 
 # Use find to copy all files except index.md
 find $src_dir -type f ! -name 'index.md' ! -name .git ! -name .gitignore -exec cp {} $dest_dir \;
-
-# Then use find to delete the copied files from the source directory
-find $src_dir -type f ! -name 'index.md' ! -name .git ! -name .gitignore -exec rm {} \;
