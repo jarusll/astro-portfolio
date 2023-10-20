3a3006106725c342c21bd264b7b4b428087a6d15
Suraj Yadav <ysuraj877@gmail.com>
2023-10-20 15:50:06 +0000

    Remove integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
-import purgecss from "astro-purgecss";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
-  integrations: [purgecss()]
 });
