2ec50b84c5b641102fc757eab4155dcd7d92453a
Suraj Yadav <ysuraj877@gmail.com>
2023-10-20 08:35:20 +0000

    Compress assets using webp

--- scripts/copy_assets_from_diary.sh
+++ scripts/copy_assets_from_diary.sh
@@ -8,3 +8,11 @@ make -p ./public
 
 # Use find to copy all files except index.md
 find $src_dir -type f ! -name 'index.md' ! -name .git ! -name .gitignore -exec cp {} $dest_dir \;
+
+# loop & compress images
+find $dest_dir -type f \( -name "*.png" -o -name "*.jpg" -o -name "*.jpeg" \) | parallel cwebp {} -o {}
+# for file in $dest_dir/*; do
+#   if [[ $file =~ \.(png|jpg|jpeg) ]]; then
+#     cwebp $file -o $file -q 50
+#   fi
+# done
