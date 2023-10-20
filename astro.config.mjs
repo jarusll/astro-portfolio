28573e177ec9d5f4b266e8d98a2ed898829bad05
Suraj Yadav <ysuraj877@gmail.com>
2023-10-20 08:35:10 +0000

    Remove purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [purgecss()],
   output: 'server',
   adapter: node({
     mode: 'standalone',
