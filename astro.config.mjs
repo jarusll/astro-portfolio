361c5cf3f104402ae6b865035f0a719ee274d649
Suraj Yadav <ysuraj877@gmail.com>
2023-12-10 18:07:03 +0000

    Remove 443 port

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  server: {
-    port: 443
-  },
   adapter: node({
     mode: 'standalone'
   }),
