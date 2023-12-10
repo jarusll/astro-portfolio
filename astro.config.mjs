08e731f98eb91e611efff1cb230d6b2bab1de8aa
Suraj Yadav <ysuraj877@gmail.com>
2023-12-10 18:02:11 +0000

    Change default port

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  server: {
+    port: 443
+  },
   adapter: node({
     mode: 'standalone'
   }),
