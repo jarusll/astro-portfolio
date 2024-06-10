2995e969655572c1b5f84d46ccfee89c963389fa
Suraj Yadav <ysuraj877@gmail.com>
2024-06-10 20:51:08 +0530

    fix: :fire: Remove unneeded import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
