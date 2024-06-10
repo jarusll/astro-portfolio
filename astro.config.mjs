ddab8e8ed41d0808f7684f7b0f453b9237c1b85f
Suraj Yadav <ysuraj877@gmail.com>
2024-06-10 20:27:44 +0530

    feat: :mag: Use sitemap plugin and add sitemap.xml in head tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
+  site: 'https://suraj.lol',
+  integrations: [sitemap()]
 });
